0064383ac087d68239e4bc3c7355f374392626c5
Matias Schmidt <matias.schmidt@outlook.es>
2022-08-22 23:17:12 -0300

    first commit

---
+++ src/components/Cards.js
@@ -0,0 +1,12 @@
+import './Cards.css';
+
+export default function CardThanks({ resetForm }) {
+	return (
+		<div className='cardThanks'>
+			<img src="icon-complete.svg" alt="" />
+			<p>Thank you!</p>
+			<p>We've added your card details</p>
+			<button className='btn-primary' onClick={resetForm}>Continue</button>
+		</div>
+	);
+}
